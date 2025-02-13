aed966bda40b5b468b7e4d15e243882445456f43
@brody4hire - C. Jonathan Brody <brody4hire@brodycj.com>
2025-02-12 23:41:43 -0500

    add portable_atomic_arc cfg option (etc.) & add documentation

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # portable-arc
 
+<!-- NOTE: DO `cargo install cargo-sync-readme` then `cargo sync-readme` to synchronize with doc in lib.rs  -->
+<!-- cargo-sync-readme start -->
+
+This crate exports `Arc` type alias which is either of:
+
+- `Arc` from `portable-atomic-util` - enabled if the following requirements are met:
+  - use RUSTFLAGS with `--cfg portable_atomic_arc`
+  - enable `critical-section` crate feature
+- otherwise: `alloc::sync::alloc::sync::Arc`
+
+<!-- cargo-sync-readme end -->
+
 ## License
 
 Either of the following options:
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,24 @@
+//! This crate exports `Arc` type alias which is either of:
+//!
+//! - `Arc` from `portable-atomic-util` - enabled if the following requirements are met:
+//!   - use RUSTFLAGS with `--cfg portable_atomic_arc`
+//!   - enable `critical-section` crate feature
+//! - otherwise: `alloc::sync::alloc::sync::Arc`
+
 #![no_std]
+
+#[cfg(all(portable_atomic_arc, not(feature = "critical-section")))]
+compile_error!("portable_atomic_arc cfg flag requires critical-section feature to be enabled");
+
+#[cfg(all(feature = "critical-section", not(portable_atomic_arc)))]
+compile_error!("critical-section feature requires RUSTFLAGS with `--cfg portable_atomic_arc`");
 
 extern crate alloc;
 
+#[cfg(portable_atomic_arc)]
+/// `Arc` type alias - see crate documentation for more information
+pub type Arc<T> = portable_atomic_util::Arc<T>;
+
+#[cfg(not(portable_atomic_arc))]
+/// `Arc` type alias - see crate documentation for more information
 pub type Arc<T> = alloc::sync::Arc<T>;
